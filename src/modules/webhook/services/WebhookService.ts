c73a16b862890ad361aeacc2d4dc2e58951e793d
Khalleb <khallebw@gmail.com>
2021-10-04 17:01:07 -0300

    mprint

--- src/modules/webhook/services/WebhookService.ts
+++ src/modules/webhook/services/WebhookService.ts
@@ -19,6 +19,7 @@ class WebhookService {
       if (!request) {
         throw new AppError(i18n('webhook.request_not_found'));
       }
+      Logger.info(request);
       if (!request?.body) {
         throw new AppError(i18n('webhook.request_not_found_body'));
       }
