4ae00d14d0be53e22b52291f234685487e7ae978
Khalleb <khallebw@gmail.com>
2021-10-04 17:09:04 -0300

    mprint

--- src/modules/webhook/infra/http/routes/webhook.routes.ts
+++ src/modules/webhook/infra/http/routes/webhook.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
+import { text } from 'body-parser';
+
 import WebHookController from '../controllers/WebHookController';
 
 const router = Router();
 const controller = new WebHookController();
 
-router.post('/events/notifications', controller.handle);
+router.post('/events/notifications', text(), controller.handle);
 export default router;
